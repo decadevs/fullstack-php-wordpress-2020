2e0946153c49f5f5e8d53fd5a04a1414c5ffbb58
deyems <adeyemisola11@gmail.com>
2020-11-05 11:45:27 +0100

    fix empty fields

--- week1/tue/assignment/save.php
+++ week1/tue/assignment/save.php
@@ -1,25 +1,43 @@
+    <?php
+        $errors = [];
+    ?>
+   
 <?php
-
     if(isset($_POST["add"])){
-        $firstname = mysqli_escape_string($_POST["firstname"]);
-        $lastname = mysqli_escape_string($_POST["lastname"]);
-        $member = [
-            'last_name' => $firstname,
-            'first_name' => $lastname
-        ];
+        $firstname = htmlspecialchars($_POST["firstname"]);
+        $lastname = htmlspecialchars($_POST["lastname"]);
+        if($firstname === ""){
+            $errors[] = "Firstname is empty";
+        }
+        if($lastname === ""){
+            $errors[] = "Lastname is empty";
+        }
 
-        $filename = 'database.json';
-        $inp = file_get_contents($filename);
-        if($inp === ""){
-            $tempArray = [];
-            array_push($tempArray, $member);
-            $jsonData = json_encode($tempArray);
-            file_put_contents($filename, $jsonData);
-        }else{
-            $tempArray = json_decode($inp);
-            array_push($tempArray, $member);
-            $jsonData = json_encode($tempArray);
-            file_put_contents($filename, $jsonData);
+        if(!empty($errors)){
+            foreach ($errors as $key => $error) {
+                echo $error;
+            }
+        }
+
+        if(empty($errors)){
+            $member = [
+                'last_name' => $firstname,
+                'first_name' => $lastname
+            ];
+    
+            $filename = 'database.json';
+            $inp = file_get_contents($filename);
+            if($inp === ""){
+                $tempArray = [];
+                array_push($tempArray, $member);
+                $jsonData = json_encode($tempArray);
+                file_put_contents($filename, $jsonData);
+            }else{
+                $tempArray = json_decode($inp);
+                array_push($tempArray, $member);
+                $jsonData = json_encode($tempArray);
+                file_put_contents($filename, $jsonData);
+            }
         }
     }
     
