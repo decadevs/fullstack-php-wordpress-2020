590c8aa2c95d5ed4cb2027ba6c745dd42d1bf090
deyems <adeyemisola11@gmail.com>
2020-11-08 22:58:26 +0100

    added script to prevent reload using fetch

--- week1/thu/assignment/mainscript.js
+++ week1/thu/assignment/mainscript.js
@@ -1,6 +1,9 @@
 
 let figures = document.querySelectorAll(".unique-img");
 let closeBtns = document.querySelectorAll(".close-btn");
+const fileField = document.querySelector('input[type="file"]');
+const submitBtn = document.querySelector('button[type="submit"]');
+const formElement = document.querySelector(".forminput");
 
 figures.forEach((figure,idx) => {
     figure.addEventListener("click", (e) => {
@@ -14,3 +17,37 @@ figures.forEach((figure,idx) => {
     });
 
 });
+formElement.addEventListener("submit", (e) => {
+    // console.log(e);
+    e.preventDefault();
+    console.log(fileField.files[0]);
+    console.log(typeof fileField.files[0]);
+    console.log(fileField);
+    // console.log(formElement);
+    
+    const formData = new FormData(formElement);
+    formData.append('photo', fileField.files[0]);
+    // console.log(JSON.stringify(formData));
+    console.log(formData);
+    fetch("gallery.php",{
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+    })
+    .then(response => {
+        console.log(response);
+        return response.url;
+    })
+    .then(result => {
+        console.log('Success: ', result);
+        // window.location(result.url);
+    })
+    .catch(err => {
+        console.error('Error: ', err);
+    });
+})
+// window.onload(e => {
+//     console.log(e);
+// })
