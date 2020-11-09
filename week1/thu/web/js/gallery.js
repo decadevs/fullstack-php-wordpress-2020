dd474e2aac2991ee16c6abf2a4dfc2c8c0ea4102
Olumaja <olumajasegun@gmai.com>
2020-11-09 17:27:22 +0100

    Eventlistener added

---
+++ week1/thu/web/js/gallery.js
@@ -0,0 +1,9 @@
+/**
+ * Created by hp on 11/9/2020.
+ */
+
+let imgModel = document.querySelector(".contentwrapper");
+
+imgModel.addEventListener('click', function(e){
+    alert("You clicked me!");
+});
--- week1/thu/web/photogallery.php
+++ week1/thu/web/photogallery.php
@@ -94,12 +94,14 @@ if(isset($_FILES['photofile'])){
                     }
 
                     if($validImages){
+                        $count = 0;
                         foreach($validImages as $theImage){
                             $rawInfo = pathinfo($theImage);
                             $modelInfo = $rawInfo['filename'];
                             $characterIndex = strpos($modelInfo, '_');
                             $modelName = substr($modelInfo, $characterIndex + 1);
-                            echo '<div class="imgDisplay"><div class="contentwrapper"><div class="wrappers"><img src="'.PHOTO_DIR.$theImage.'" alt="'.str_replace("_", " ", $modelName).'"></div><div class="wrappers">'.str_replace("_", " ", $modelName).'</div></div></div>';
+                            $count++;
+                            echo '<div class="imgDisplay"><div id="'.$modelName.'_'.$count.'" class="contentwrapper" data-toggle="modal" data-target="galleryModal"><div class="wrappersPix"><img src="'.PHOTO_DIR.$theImage.'" alt="'.str_replace("_", " ", $modelName).'"></div><div class="wrappers">'.str_replace("_", " ", $modelName).'</div></div></div>';
                         }
                     }
                 }
@@ -108,6 +110,8 @@ if(isset($_FILES['photofile'])){
         ?>
     </div>
 
+    <!--Modal goes in here-->
+
 </div>
 
 <!-- Optional JavaScript; choose one of the two! -->
@@ -115,7 +119,7 @@ if(isset($_FILES['photofile'])){
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+<script src="js/gallery.js"></script>
 <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
