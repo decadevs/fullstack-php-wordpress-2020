c50a7ad3efd3d93a16e50adc4d9be0e6a98ca992
deyems <adeyemisola11@gmail.com>
2020-11-18 21:59:07 +0100

    500 error page handler

--- week2/thu/blog/500.php
+++ week2/thu/blog/500.php
@@ -1,25 +1,20 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>XiReader</title>
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&display=swap" rel="stylesheet">
-    
-<link rel="stylesheet" href="assets/css/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+    <title>XiReader</title>
 </head>
 <body>
-
-
-<section class="container section">
-    <div class="post">
-        <h1 class="post-title">500 Internal server error</a></h1>
-        <p class="post-content">
-            <?php echo $errorMessage; ?>
-        </p>
-    </div>
-</section>
-    
+    <section class="container section">
+        <div class="post">
+            <h1 class="post-title">
+                500 internal server error
+            <p class="post-content"><?php echo $errorMessage ?></p>
+        </div>
+        
+    </section>
 </body>
 </html>
