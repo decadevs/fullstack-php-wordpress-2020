73c1c340e29ff3db36f49bf587e795bc5de8d677
Olumaja <olumajasegun@gmai.com>
2020-11-24 10:40:05 +0100

    debugging

--- week3/tue/js/connectwebsite.js
+++ week3/tue/js/connectwebsite.js
@@ -1,9 +1,11 @@
 let btnConnect = document.querySelector('.btn-website');
+let modal = document.querySelector('.modal-bg');
+let closeModal = document.querySelector('.close-modal');
 
 btnConnect.addEventListener('click', function(){
-    modal.classList.add('activate-modal')
+    modal.classList.add('activate-modal');
 });
 
 closeModal.addEventListener('click', function(){
-    modal.classList.remove('activate-modal')
+    modal.classList.remove('activate-modal');
 });
