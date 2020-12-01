5bf5817471c9643c9e6ac5d2c7eca3733a63b7ab
mystevotgit <oyeolatunji@gmail.com>
2020-12-01 09:59:10 +0100

    routing

---
+++ week4/thu/homepage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to our site</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+
+    <form action="">
+        <input type="text" name="email" id="">
+        <input type="text" name="phone" id="">
+        <input type="text" name="name" id="">
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
--- week4/thu/public/index.php
+++ week4/thu/public/index.php
@@ -1,3 +1,51 @@
 <?php
 
-echo "Welcome to our programe";
+
+class HomepageController {
+
+    public static function index($greeting) {
+        echo $greeting. ', joe';
+    }
+}
+
+
+function getCleanURI(): string {
+    $uri = isset($_SERVER['PHP_SELF']) ? trim($_SERVER['PHP_SELF']) : '/';
+    $uri = str_ireplace('index.php', '', $uri);
+    $uri = trim($uri, '/');
+
+    
+    
+    return $uri;
+}
+
+
+function homePage() {
+    include_once '../homepage.php';
+}
+
+
+function usersApi() {
+    header('Content-type: application/json');
+    echo json_encode($_GET);
+    exit;
+
+}
+
+$uri = getCleanURI();
+
+
+$routes = [
+    '' => 'homePage',
+    'api' => 'usersApi'
+];
+
+/*foreach($routes as $url=>$handler) {
+    if($uri === $url) {
+        call_user_func($handler);
+        exit;
+    }
+}*/
+
+$homepage = new HomepageController;
+call_user_func([$homepage, 'index'], 'Hi');
