25cd996d52dccda13a7f86d7f09cdb9adf903979
Olumaja <olumajasegun@gmai.com>
2020-12-03 00:10:15 +0100

    last commit

--- week4/thu/public/index.php
+++ week4/thu/public/index.php
@@ -5,17 +5,17 @@
     //(new WP_App\Application())->mount();
 
     $uri = getURI();
-    echo $uri;
+    //echo $uri;
     $routes = ['signin' => "homepage", 'register' => 'signup', 'dashboard' => 'dashboard'];
     $app = new WP_App\Application();
 
     foreach($routes as $route=>$link){
-        if($link === $uri){
+        if($route === $uri){
             call_user_func([$app, $link]);
             exit;
         }
-
     }
+        echo '404 Not found';
     
     function getURI() : string{
         $uri = isset($_SERVER['PHP_SELF']) ? trim($_SERVER['PHP_SELF']) : '/';
