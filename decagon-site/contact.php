6dc2225122ed9f556ea3aace7758b7535fa23033
VivianObiako <vivianobiako1@gmail.com>
2020-12-14 11:01:02 +0100

    added contact-page

---
+++ decagon-site/contact.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DecagonHq</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    
+
+</head>
+<body>
+    <div class="body">
+        <header class="header bg-dark-contact">
+            
+            <nav>
+                <div class="dextop-padding">
+                    <div class="container">
+                        <div class="menu">
+                                <div class="logo"><a href="#"><img src="images/logo.svg" alt=""></a></div>
+                                <div class="links">
+                                    <div class="item"><a href="#">For Developers</a></div> 
+                                    <div class="item"><a href="#">Blog</a></div>
+                                    <div class="item button"><a href="#">Get started</a></div>
+                                    <div class="toggle"><a href="#"><i class="fas fa-bars"></i></a></div>
+                                </div>
+                        </div>    
+                    </div>
+                </div>
+            </nav>
+            
+            <div class="page-intro">
+                <div class="container">
+                    <div class="intro-text intro-text-contact">
+                        <h1 class="intro-header"><span class="mobile-text">Build your </span><span class="dextop-text">dependable</span></h1> 
+                        <h1 class="intro-header">remote engineering team.</h1>
+                        <p class="intro-pgrph first">Provide your details below and we will follow up in less than 24Hrs.</p>
+                
+                        <div class="form-control">
+                            <form action="" method="post">
+                                <div class="form-input">
+                                    <label for="text" class="form-title">Your name is</label>
+                                    <input type="text" name="name" id="text" placeholder="Enter full name here">
+                                </div>
+                                <div class="form-shared-line">
+                                    <div class="form-input first">
+                                        <label for="role-text" class="form-title">Your role is</label>
+                                        <input type="text" name="role" id="role-text" placeholder="e.g CTO">
+                                    </div>
+                                    <div class="form-input second">
+                                        <label for="company-text" class="form-title">At which company</label>
+                                        <input type="text" name="company" id="company-text" placeholder="e.g Google">
+                                    </div>
+                                </div>
+                                <div class="form-input">
+                                        <label for="country-text" class="form-title">Your operate from</label>
+                                        <input type="country-text" name="country" id="country-text" placeholder="Choose your country">
+                                </div>
+                                <div class="form-input">
+                                        <label for="engineers" class="form-title">Engineer needs</label>
+                                        <input type="text" name="engineers" id="engineers" placeholder="Specify how many engineers">
+                                </div>
+                                <div class="form-input">
+                                        <label for="email" class="form-title">Email address</label>
+                                        <input type="email" name="email" id="email" placeholder="Enter email address here">
+                                </div>
+                                <div class="form-input form-submit btn">
+                                    <button type="submit" class="contact-btn">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+        <div class="clients">
+            <div class="dextop-padding">
+                <div class="container">
+                    <div class="clients-content">
+                        <p class="clients-title">Our Engineers have built products for</p>
+                        <span class="client-logos">
+                            <img src="images/clients.svg" alt="Merk's Logo">
+                            <!-- <img src="images/merk.svg" alt="Merk's Logo">
+                            <img src="images/nike.svg" alt="Nike's logo">
+                            <img src="images/mastercard.svg" alt="mastercard's logo">
+                            <img src="images/vertex.svg" alt="vertex logo">
+                            <img src="images/bud.svg" alt="budweiser's logo">
+                            <img src="images/nokia.svg" alt="Nokia's logo"> -->
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ decagon-site/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DecagonHq</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    
+
+</head>
+<body>
+    <div class="body">
+        <header class="header bg-dark">
+            
+            <nav>
+                <div class="dextop-padding">
+                    <div class="container">
+                        <div class="menu">
+                                <div class="logo"><a href="#"><img src="images/logo.svg" alt=""></a></div>
+                                <div class="links">
+                                    <div class="item"><a href="#">For Developers</a></div> 
+                                    <div class="item"><a href="#">Blog</a></div>
+                                    <div class="item button"><a href="#">Get started</a></div>
+                                    <div class="toggle"><a href="#"><i class="fas fa-bars"></i></a></div>
+                                </div>
+                        </div>    
+                    </div>
+                </div>
+            </nav>
+            
+            <div class="page-intro">
+                <div class="container">
+                    <div class="intro-text">
+                        <h1 class="intro-header"><span class="mobile-text">Build faster with </span><span class="dextop-text">vetted</span> remote</h1> 
+                        <h1 class="intro-header">software engineering talent.</h1>
+                        <p class="intro-pgrph first">Recruiting, training, HR management and work infrastructure</p>
+                        <p class="intro-pgrph">to help you scale quickly and cost effectively.</p>
+                        <div class="intro-btn">
+                            
+                            <a href="contact.php"><button class="btn btn-nav">Build My Team</button></a>
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+        <div class="clients">
+            <div class="dextop-padding">
+                <div class="container">
+                    <div class="clients-content">
+                        <p class="clients-title">Our Engineers have built products for</p>
+                        <span class="client-logos">
+                            <img src="images/clients.svg" alt="Merk's Logo">
+                            <!-- <img src="images/merk.svg" alt="Merk's Logo">
+                            <img src="images/nike.svg" alt="Nike's logo">
+                            <img src="images/mastercard.svg" alt="mastercard's logo">
+                            <img src="images/vertex.svg" alt="vertex logo">
+                            <img src="images/bud.svg" alt="budweiser's logo">
+                            <img src="images/nokia.svg" alt="Nokia's logo"> -->
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
