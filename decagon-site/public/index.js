c1975ea94b1527ae49a49c00e2e6b74f410058f9
mystevotgit <oyeolatunji@gmail.com>
2020-12-20 10:31:30 +0100

    responsive desktop and mobile view

--- decagon-site/public/index.js
+++ decagon-site/public/index.js
@@ -1,8 +1,23 @@
 $(function () {
     $(document).scroll(function () {
-        console.log("hello")
       var $nav = $(".navbar");
-      console.log($(this).scrollTop())
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+    
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+
   });
